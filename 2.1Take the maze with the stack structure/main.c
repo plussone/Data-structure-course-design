a639288a4aa8da841750b20e0fe3f996d681f381
plussone <49369205+plussone@users.noreply.github.com>
2019-09-24 23:27:13 +0800

    Update main.c

--- 2.1Take the maze with the stack structure/main.c
+++ 2.1Take the maze with the stack structure/main.c
@@ -1,52 +1,52 @@
-/*���ݽṹ�ڶ�����ҵ��ʵ�̶ֹ���ͼѰ�ҳ���·��
-	18184204-������  */
+/*数据结构第二次作业：实现固定地图寻找出口路线
+	plussone  */
 #define _CRT_SECURE_NO_WARNINGS
 
-#define STACK_INIT_SIZE 100//ջ�������ռ���
-#define STACKINCREMENT 10//����ռ������
+#define STACK_INIT_SIZE 100//栈最初分配空间量
+#define STACKINCREMENT 10//储存空间分配量
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-int map[10000][10000], vis[10000][10000], dir[5][2] = { {0,0},{0,-1},{0,1},{1,0},{-1,0} }; //��ͼ����ǡ���������
-int a = 10, b = 10; //��ͼ�Ŀ��ͳ�
-int x1 = 1, y1 = 0, x2 = 1, y2 = 9; //�����յ�λ��
+int map[10000][10000], vis[10000][10000], dir[5][2] = { {0,0},{0,-1},{0,1},{1,0},{-1,0} }; //地图、标记、方向数组
+int a = 10, b = 10; //地图的宽和长
+int x1 = 1, y1 = 0, x2 = 1, y2 = 9; //起点和终点位置
 
-struct Node //�Թ��ڵ�
+struct Node //迷宫节点
 {
-	int x; //x����
-	int y; //y����
-	int dirn; //������
+	int x; //x坐标
+	int y; //y坐标
+	int dirn; //来方向
 };
 
 struct SqStack
 {
-	struct Node* base; //ջ��ָ��
-	struct Node* top; //ջ��ָ��
-	int stacksize; //��������
+	struct Node* base; //栈顶指针
+	struct Node* top; //栈顶指针
+	int stacksize; //储存数据
 };
 
-int InitStack(struct SqStack* S) //��ʼ����ջS
+int InitStack(struct SqStack* S) //初始化空栈S
 {
 	S->base = (struct Node*)malloc(STACK_INIT_SIZE * sizeof(struct Node));
 	if (!S->base)
 	{
-		return 0; //�ռ����ʧ��
+		return 0; //空间分配失败
 	}
 	S->stacksize = STACK_INIT_SIZE;
 	S->top = S->base;
 	return 1;
 }
 
-int Push(struct SqStack* S, int x, int y, int d) //��ջ��������Ԫ��
+int Push(struct SqStack* S, int x, int y, int d) //向栈顶插入新元素
 {
 	if ((S->top) - (S->base) >= S->stacksize)
 	{
-		S->base = (struct Node*)realloc(S->base, (S->stacksize + STACKINCREMENT) * sizeof(struct Node)); //ջ����׷�Ӵ洢�ռ�
+		S->base = (struct Node*)realloc(S->base, (S->stacksize + STACKINCREMENT) * sizeof(struct Node)); //栈满，追加存储空间
 		if (!S->base)
 		{
-			return 0; //����ʧ��
+			return 0; //分配失败
 		}
 		S->top = S->base + S->stacksize;
 		S->stacksize += STACKINCREMENT;
@@ -58,11 +58,11 @@ int Push(struct SqStack* S, int x, int y, int d) //��ջ��������Ԫ��
 	return 1;
 }
 
-struct Node* Gettop(struct SqStack* S) //���ջ����ֵ
+struct Node* Gettop(struct SqStack* S) //输出栈顶的值
 {
 	if (S->top == S->base)
 	{
-		return NULL; //ջΪ��
+		return NULL; //栈为空
 	}
 	else
 	{
@@ -70,35 +70,35 @@ struct Node* Gettop(struct SqStack* S) //���ջ����ֵ
 	}
 }
 
-struct Node* Pop(struct SqStack* S) //ɾ��ջ��Ԫ��,�������ֵ
+struct Node* Pop(struct SqStack* S) //删除栈顶元素,并输出其值
 {
 	if (S->top == S->base)
 	{
-		return NULL; //ջΪ��
+		return NULL; //栈为空
 	}
 	return --(S->top);
 }
 
-void SqStack_print(struct SqStack* S) //����Թ�·��
+void SqStack_print(struct SqStack* S) //输出迷宫路线
 {
-	struct SqStack* L = (struct SqStack*)malloc(sizeof(struct SqStack)); //����һ����
-	if (!InitStack(L)) //��ʼ���±�L
+	struct SqStack* L = (struct SqStack*)malloc(sizeof(struct SqStack)); //建立一个堆
+	if (!InitStack(L)) //初始化新表L
 	{
-		printf("������ջʧ��\n");
+		printf("建立空栈失败\n");
 		return;
 	}
-	struct Node* now1 = (struct Node*)malloc(sizeof(struct Node)); //����һ����ʱ����ṹ��
+	struct Node* now1 = (struct Node*)malloc(sizeof(struct Node)); //建立一个临时坐标结构体
 	while (Gettop(S))
 	{
 		//printf("100");
 		now1 = Pop(S);
 		if (!Push(L, now1->x, now1->y, 0))
 		{
-			printf("������ջʧ��\n");
+			printf("添加新栈失败\n");
 			return;
 		}
 	}
-	printf("�Թ�·��Ϊ��");
+	printf("迷宫路线为：");
 	while (Gettop(L))
 	{
 		now1 = Pop(L);
@@ -114,11 +114,11 @@ void makemap()
 
 void sacnfdata()
 {
-	printf("�������Թ��ĳ��Ϳ���");
+	printf("请输入迷宫的长和宽：");
 	scanf("%d%d", &b, &a);
-	printf("���������λ�ã�");
+	printf("请输入起点位置：");
 	scanf("%d%d", &x1, &y2);
-	printf("�������յ�λ�ã�");
+	printf("请输入终点位置：");
 	scanf("%d%d", &x2, &y2);
 	system("cls");
 }
@@ -137,43 +137,43 @@ int main()
 			}
 			else
 			{
-				printf("��");
+				printf("█");
 			}
 		}
 		printf("\n");
 	}
-	struct SqStack* S = (struct SqStack*)malloc(sizeof(struct SqStack)); //����һ��ջ��
-	memset(vis, 0, sizeof(vis)); //��ʼ���������
-	if (!InitStack(S)) //��ʼ���±�L
+	struct SqStack* S = (struct SqStack*)malloc(sizeof(struct SqStack)); //建立一个栈堆
+	memset(vis, 0, sizeof(vis)); //初始化标记数组
+	if (!InitStack(S)) //初始化新表L
 	{
-		printf("������ջʧ��\n");
+		printf("建立空栈失败\n");
 		return 0;
 	}
-	struct Node* now = (struct Node*)malloc(sizeof(struct Node)); //������ʱ����ṹ��
+	struct Node* now = (struct Node*)malloc(sizeof(struct Node)); //建立临时坐标结构体
 	now->x = x1;
 	now->y = y1;
-	if (!Push(S, x1, y1, 1)) //��ڽ�ջ
+	if (!Push(S, x1, y1, 1)) //入口进栈
 	{
-		printf("������ջʧ��\n");
+		printf("添加新栈失败\n");
 		return 0;
 	}
-	int flag = 0; //����·�߱�־
+	int flag = 0; //有无路线标志
 	while (Gettop(S))
 	{
 		now = Gettop(S);
-		if (now->x == x2 && now->y == y2) //�ҵ����ڽ���
+		if (now->x == x2 && now->y == y2) //找到出口结束
 		{
 			flag = 1;
 			break;
 		}
-		else if (vis[now->x][now->y] == 5) //�Ҳ�����һ��Ԫ�ط���
+		else if (vis[now->x][now->y] == 5) //找不到下一个元素返回
 		{
 			Pop(S);
 			continue;
 		}
 		else
 		{
-			int d = 0; //������
+			int d = 0; //来方向
 			if (vis[now->x][now->y] == 1)
 			{
 				d = 2;
@@ -191,21 +191,21 @@ int main()
 				d = 3;
 			}
 			//printf("%d %d\n", now->x + dir[vis[now->x][now->y]][0], now->y + dir[vis[now->x][now->y]][1]);
-			if (now->x + dir[vis[now->x][now->y]][0] < 0 || now->x + dir[vis[now->x][now->y]][0] >= a || now->y + dir[vis[now->x][now->y]][1] < 0 || now->y + dir[vis[now->x][now->y]][1] >= b) //�±�Խ��
+			if (now->x + dir[vis[now->x][now->y]][0] < 0 || now->x + dir[vis[now->x][now->y]][0] >= a || now->y + dir[vis[now->x][now->y]][1] < 0 || now->y + dir[vis[now->x][now->y]][1] >= b) //下标越界
 			{
 				vis[now->x][now->y]++;
 				continue;
 			}
-			else if (map[now->x][now->y] == 1 || now->dirn == vis[now->x][now->y] || vis[now->x][now->y] == 0) //����ǽ���ػ����������򷵻ػ�ԭ�ز���
+			else if (map[now->x][now->y] == 1 || now->dirn == vis[now->x][now->y] || vis[now->x][now->y] == 0) //遇到墙返回或遇到来方向返回或原地不动
 			{
 				//printf("1000\n");
 				vis[now->x][now->y]++;
 				continue;
 			}
-			else if (!Push(S, now->x + dir[vis[now->x][now->y]][0], now->y + dir[vis[now->x][now->y]][1], d)) //������һ������
+			else if (!Push(S, now->x + dir[vis[now->x][now->y]][0], now->y + dir[vis[now->x][now->y]][1], d)) //插入下一个方向
 			{
 				vis[now->x][now->y]++;
-				printf("������ջʧ��\n");
+				printf("添加新栈失败\n");
 				return 0;
 			}
 			//printf("%d %d\n", now->x, now->y);
@@ -222,13 +222,13 @@ int main()
 	}*/
 	if (flag == 0)
 	{
-		printf("�Թ�û��ͨ·\n");
+		printf("迷宫没有通路\n");
 	}
 	else
 	{
 		SqStack_print(S);
 	}
-	free(S->base); //�ͷ�ջ�ڴ�ռ�
+	free(S->base); //释放栈内存空间
 	free(S);
 	return 0;
 }
